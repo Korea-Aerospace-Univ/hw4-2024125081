57a8893625a6a4721ca3ed5ef036be0ac1804fb5
2024125081 <seongjin1623532@gmail.com>
2026-04-20 15:23:55 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -14,9 +14,9 @@ int main()
         count++;
         
     if (answer > what) {
-        printf("%d보다 낮습니다\n", what);
+        printf("%d 보다 낮습니다\n", what);
     }
-    else if (answer < what) { printf("%d보다 높습니다\n", what); }
+    else if (answer < what) { printf("%d 보다 높습니다\n", what); }
     else { printf("%d 정답입니다\n", what); }
     }
     while (what != answer);
