61219da8570bbc8ccac1368c8b24b1b486028d90
2024125081 <seongjin1623532@gmail.com>
2026-04-20 16:03:21 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,23 +2,24 @@
 
 int main()
 {
-    int answer;
-    int question;
+    int answer = 10;
+    int question = 0;
     int count = 0;
     
     scanf("%d", &answer);
     
-    do
-    {
+    do {
         scanf("%d", &question);
         count++;
         
     if (answer > question) {
-        printf("%d>?\n", question);
+        printf("%d<?\n", question);
     }
-    else if (answer < question) { printf("%d<?\n", question); } 
-    else { printf("%d==?", question); 
-
+    else if (answer < question) { 
+        printf("%d>?\n", question); 
+    } 
+    else {
+        printf("%d==?", question); 
         printf("시도횟수는 %d회\n", count);
     }
     while (question != answer);
