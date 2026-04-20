a99bb079752bd1e97e0f5d890a65c556bb628938
2024125081 <seongjin1623532@gmail.com>
2026-04-20 16:47:11 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int answer = 10;
-    int question = 0;
-    int count = 0;
+    int answer = 10;                 //정답값 변수 초기화
+    int question = 0;                  //추측값 변수 초기화
+    int count = 0;                       // 시도 횟수 
     
-    scanf("%d", &answer);
+    scanf("%d", &answer);              // 정답값 정수 입력받기 
     
-    do {
-        scanf("%d", &question);
-        count++;
+    do {                                          //추측값 정수 입력받기 
+        scanf("%d", &question); 
+        count++;                                    // 시도 횟수 증가   
         
-        if (answer > question) {
+        if (answer > question) {                         //정답값이 추측값보다 클 경우 실행
             printf("%d<?\n", question);
         }
-        else if (answer < question) { 
+        else if (answer < question) {                      //정답값보다 추측값이 클 경우 실행
             printf("%d>?\n", question); 
         } 
-        else {
-            printf("%d==?", question); 
-            printf("%d", count);
+        else {                                               //else이니 두 경우 모두 아닌 정답값과 추측값이 같으면 실행
+            printf("%d==?\n", question); 
+            printf("%d\n", count);                              
         }
     }
-    while (question != answer);
+    while (question != answer);                                   //정답값과 추측값이 같지 않을 경우 반복해서 실행
     
     
     return 0;
